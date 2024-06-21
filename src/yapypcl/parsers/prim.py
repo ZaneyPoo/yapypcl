120494e39d66e0ac0a93ae21177cf9f430139da2
Zane <zanehargis@gmail.com>
2024-06-21 13:54:19 -0700

    ref(parsers.prim): grab first char of string always + docstring

--- src/yapypcl/parsers/prim.py
+++ src/yapypcl/parsers/prim.py
@@ -3,6 +3,10 @@ yapypcl -- prim.py
 
 Primitive parsers
 
+All of these parsers default to matching exactly once.
+In order to match an arbitrary number of occurrences,
+compose them with a combinator.
+
 Zane Hargis -- Copyright 2024
 """
 
@@ -10,8 +14,16 @@ from .parser import ParseErr, Parser, ParseResult
 
 
 def char[T](ch: str) -> Parser[T]:
+    """
+    Return a parser which matches a specific character.
+
+    Examples: 
+    >>> parse(char('a'), "abc")
+    Ok((['a'], 'bc'))
+    """
+    ch = ch[0]
     def parser(text: str) -> ParseResult[T]:
-        if text[0] == ch[0]:
+        if text[0] == ch:
             return ParseResult.make_ok(([ch], text[1:]))
         else:
             return ParseResult.make_err(ParseErr.expected_char(ch, text[0]))
